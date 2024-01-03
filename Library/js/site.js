443c07130dc89aab09086fc4d6432ad72dc567b4
Murillo Grubler <msgrubler@gmail.com>
2024-01-03 20:40:48 +0100

    feat: remove jquery library

--- Library/js/site.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	$(".open-modal").on("click", function(){
-		$("#modal").show("fast");
-	});
-	
-	$(".img-close-modal").on("click", function(){
-		$("#modal").hide("fast");
-	});
-});
