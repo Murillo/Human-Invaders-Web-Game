5ccf3bf6c388d6c62939715d4f120f8019628e70
Murillo Grubler <msgrubler@gmail.com>
2024-03-11 00:23:59 +0100

    feat: remove option button and fix about modal

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { Game } from './Game';
 
+// Start the game
 var startButton = document.querySelector('.nav.navbar-nav li a');
 startButton.addEventListener('click', function() {
 	var parentElement = startButton.parentNode;
@@ -8,8 +9,27 @@ startButton.addEventListener('click', function() {
 	const game = new Game(htmlContainer);
 	game.load()
 		.then(() => {
+			closeModal();
 			game.start();
 			htmlContainer.removeAttribute('class');
 		})
 		.catch(() => console.log('Failed to start the game!'))
 });
+
+// Open the about modal
+var aboutButton = document.querySelector('.nav.navbar-nav li a.open-modal');
+aboutButton.addEventListener('click', function() {
+	var modal = document.getElementById('modal');
+	modal.style.display = 'block';
+});
+
+// Close the about modal
+var closeModalButton = document.querySelector('#modal .modal-close');
+closeModalButton.addEventListener('click', function() {
+	closeModal();
+});
+
+function closeModal() {
+	var modal = document.getElementById('modal');
+	modal.style.display = 'none';
+}
